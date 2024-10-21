6efb01507c7f22733cade0379809ca5419223b5f
SSUHYUNKIM <yellosuhyun@naver.com>
2024-10-21 11:15:14 +0900

    chore: 프로젝트 초기 설정 완료

---
+++ src/main/java/org/richardstallman/dvback/DvBackApplication.java
@@ -0,0 +1,13 @@
+package org.richardstallman.dvback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/richardstallman/dvback/DvBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.richardstallman.dvback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
