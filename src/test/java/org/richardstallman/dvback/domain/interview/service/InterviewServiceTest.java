ec9548fdba24a402fc4927c34f098d92ca034e7b
mijungpark <andbeyond0331@gmail.com>
2024-10-22 15:26:27 +0900

    [DV-29] test : 면접 직무 입력 및 조회 test 추가

---
+++ src/test/java/org/richardstallman/dvback/domain/interview/service/InterviewServiceTest.java
@@ -0,0 +1,82 @@
+package org.richardstallman.dvback.domain.interview.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+import static org.richardstallman.dvback.common.constant.CommonConstants.JobName.BACK_END;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.richardstallman.dvback.common.constant.CommonConstants.InterviewMethod;
+import org.richardstallman.dvback.common.constant.CommonConstants.InterviewMode;
+import org.richardstallman.dvback.common.constant.CommonConstants.InterviewStatus;
+import org.richardstallman.dvback.common.constant.CommonConstants.InterviewType;
+import org.richardstallman.dvback.domain.interview.domain.InterviewDomain;
+import org.richardstallman.dvback.domain.interview.domain.request.InterviewCreateRequestDto;
+import org.richardstallman.dvback.domain.interview.domain.response.InterviewCreateResponseDto;
+import org.richardstallman.dvback.domain.job.domain.JobDomain;
+import org.richardstallman.dvback.domain.job.service.JobService;
+import org.richardstallman.dvback.mock.interview.FakeInterviewRepository;
+
+@Slf4j
+public class InterviewServiceTest {
+
+  @Mock
+  private JobService jobService;
+
+  @InjectMocks
+  private InterviewServiceImpl interviewServiceImpl;
+
+  @BeforeEach
+  void init() {
+    MockitoAnnotations.openMocks(this);
+    FakeInterviewRepository fakeInterviewRepository = new FakeInterviewRepository();
+    this.interviewServiceImpl = new InterviewServiceImpl(fakeInterviewRepository, jobService);
+
+    InterviewType interviewType = InterviewType.PERSONAL;
+    InterviewMethod interviewMethod = InterviewMethod.CHAT;
+    InterviewMode interviewMode = InterviewMode.PRACTICE;
+    JobDomain jobDomain =
+        JobDomain.builder().jobId(1L).jobName(BACK_END).jobDescription("직무 설명").build();
+
+    when(jobService.findJobById(1L)).thenReturn(jobDomain);
+
+    fakeInterviewRepository.save(
+        InterviewDomain.builder()
+            .interviewType(interviewType)
+            .interviewMethod(interviewMethod)
+            .interviewMode(interviewMode)
+            .job(jobDomain)
+            .build());
+  }
+
+  @Test
+  void create_interview_by_save_interview_info() {
+    // given
+    InterviewType interviewType = InterviewType.TECHNICAL;
+    InterviewMethod interviewMethod = InterviewMethod.VIDEO;
+    InterviewMode interviewMode = InterviewMode.REAL;
+
+    InterviewCreateRequestDto interviewCreateRequestDto =
+        InterviewCreateRequestDto.builder()
+            .interviewType(interviewType)
+            .interviewMethod(interviewMethod)
+            .interviewMode(interviewMode)
+            .jobId(2L)
+            .build();
+
+    // when
+    InterviewCreateResponseDto interviewCreateResponseDto =
+        interviewServiceImpl.createInterview(interviewCreateRequestDto);
+
+    // then
+    assertThat(interviewCreateResponseDto.getInterviewId()).isEqualTo(2);
+    assertThat(interviewCreateResponseDto.getInterviewStatus()).isEqualTo(InterviewStatus.INITIAL);
+    assertThat(interviewCreateResponseDto.getInterviewType()).isEqualTo(interviewType);
+    assertThat(interviewCreateResponseDto.getInterviewMethod()).isEqualTo(interviewMethod);
+    assertThat(interviewCreateResponseDto.getInterviewMode()).isEqualTo(interviewMode);
+  }
+}
